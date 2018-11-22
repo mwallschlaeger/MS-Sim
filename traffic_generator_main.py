3ea054c0ef4cc8f656fc982d1c44630c24c32c27
tibut <marcel.wallschlaeger@tu-berlin.de>
2018-11-22 15:39:27 +0100

    minor changes, now works as a IOTDevice sending data every second

--- traffic_generator_main.py
+++ traffic_generator_main.py
@@ -1,18 +1,24 @@
 import threading, queue, socket, time, random, logging
 
-class Sender(threading.Thread):
+class IoTDevice(threading.Thread):
 
-	def __init__(self,host,port,id_str):
+	def __init__(self,host,port,device_id):
 		self.port = port
 		self.host = host
-		self.id_str = str(id_str)
+		self.device_id = device_id # bytes
 		self.running = True
-		self.msg_c = 0
+
+		self.bytes_out = 0
+		self.bytes_in = 0
+		self.msg_out = 0
+		self.msg_in = 0
+
+		padding_len =  random.randint(2545,25555)
+		self.padding =  random_string(padding_len)
 		super().__init__()
 
 	def run(self):
 		while self.running:
-			self.msg_c+=1
 			try:
 				self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 				self.s.connect((self.host, self.port))
@@ -21,39 +27,91 @@ class Sender(threading.Thread):
 				t = random.uniform(0.03,0.05)
 				time.sleep(t)
 				continue
-					
-			self.s.send(bytes(self.id_str, "utf-8"))
+			msg = self.build_msg()
+			self.bytes_out += len(msg)
+			self.msg_out+=1
+			self.s.send(msg)
+
+			# use poll  here to implement timeout function
+			response_msg = self.s.recv(4096)
+			self.msg_in+=1
+			self.bytes_in += len(response_msg)
+
 			self.s.close()
-			t = random.uniform(0.03,0.05)
+			t = random.uniform(0.9,1)
 			time.sleep(t)
 
-	def get_c(self):
-		return self.msg_c
+	def get_padding(self):
+		return self.padding
+
+	def build_msg(self):
+		request_id = random_string(16)
+		padding = self.get_padding()
+		return bytes(self.device_id + request_id + padding,"utf-8")
+		#return bytes(self.device_id + request_id,"utf-8")
+
+	def get_send_msg(self):
+		return self.msg_out
+
+	def get_recv_msg(self):
+		return self.msg_in
+
+	def get_send_bytes(self):
+		return self.msg_out
+
+	def get_recv_bytes(self):
+		return self.bytes_in
 
 	def stop(self):
-		logging.info("Stopping Load Generator {} ...".format(self.id_str))
+		logging.info("Stopping Load Generator {} ...".format(self.device_id))
 		self.running = False
+
+
+# https://stackoverflow.com/questions/976577/random-hash-in-python ported to p3
+def random_string(length):
+	import string
+	pool = string.ascii_lowercase + string.digits
+	return ''.join(random.choice(pool) for i in range(length))
 
 def main():
 	
-	duration = 20
+	duration = 30
 
 	logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
 	senders = []
-	for i in range(0,50):
-		senders.append(Sender("localhost",5090,i))
+	for i in range(0,500):
+		senders.append(IoTDevice(
+							"localhost",
+							5090,
+							random_string(12)
+							))
 
 	for sender in senders:
 		sender.start()
 
-	time.sleep(duration)
-	msg_c = 0
+	input("Press Enter to continue...")
+	#time.sleep(duration)
+	msg_send = 0
+	msg_recv = 0
+	bytes_send = 0
+	bytes_recv = 0 
 	for sender in senders:
-		msg_c += sender.get_c()
+		msg_send += sender.get_send_msg()
+		msg_recv += sender.get_recv_msg()
+
+		bytes_send += sender.get_send_bytes()
+		bytes_recv += sender.get_recv_bytes()
+
 		sender.stop()
 
-	print("send {} messages in {} seconds ...".format(str(msg_c),duration))
+	print("send messages: " + str(msg_send) + "  \n \
+		   received messeges: " + str(msg_recv) + " \n \
+		   overall MBytes send: " + str(bytes_send / 1000 / 1000) + " \n \
+		   overall MBytes recv: " + str(bytes_recv / 1000 / 1000) + " \n \
+   		   avg MBits/s out: " + str(bytes_send / 1000 / 1000 / 8 / duration) + " \n \
+		   avg MBits/s in: " + str(bytes_recv / 1000 / 1000 / 8 / duration) + " \n \
+		   benchmark duration: " + str(duration))
 
 if __name__ == '__main__':
 	main()
