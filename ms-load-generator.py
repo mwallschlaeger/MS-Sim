a50e31ac757eb7a31223aef589ce4b081396a877
tibut <marcel.wallschlaeger@tu-berlin.de>
2018-11-28 09:49:04 +0100

    fixed bug, which failed to reset counters

--- ms-load-generator.py
+++ ms-load-generator.py
@@ -125,7 +125,8 @@ def print_report(devices,duration):
 		bad_socket += device.get_bad_socket()
 		bytes_send += device.get_send_bytes()
 		bytes_recv += device.get_recv_bytes()
-
+		device.reset_report()
+		
 	logging.info("send msg: " + str(msg_send) + \
 		   ", recv msg: " + str(msg_recv) + \
 		   ", bad_sock: " + str(bad_socket) + \
@@ -133,7 +134,7 @@ def print_report(devices,duration):
 		   ", MBytes recv: " + str(round(bytes_recv / 1000 / 1000,5)) + \
    		   ", MBits/s out: " + str(round(bytes_send / 1000 / 1000 / duration,5)) + \
 		   ", MBits/s in: " + str(round(bytes_recv / 1000 / 1000 / duration,5)))
-	device.reset_report()
+		
 
 def configure_logging(debug,filename=None):
 	if filename is None:
