687c9ff8898930b2f70e418e4a2e61183a8e675a
tibut <marcel.wallschlaeger@tu-berlin.de>
2018-12-14 14:29:54 +0100

    adepted compute and proxy to interact with the new microservices:  authenticate, database and cloud-compute

--- ms-compute.py
+++ ms-compute.py
@@ -2,12 +2,17 @@
 
 import argparse, time, logging, queue, sys, random, threading, signal, os
 from listen_network_interface import ListenNetworkInterface
+from send_network_interface import SendNetworkInterface
+from load_balancer import RoundRobinLoadBalancer
 from cpu import CPU
-from process import Process
+from vm import VM
+from process import Process, ForwardingProcess
 from worker import Worker
 
 RUNNING = True # controls main loop
 ELEMENTS = []
+t_name = "MS-COMPUTE"
+
 
 def configure_logging(debug,filename=None):
 	if filename is None:
@@ -40,8 +45,16 @@ def main():
 	
 	# compute
 	parser.add_argument("-listen_port",type=int, default=5091,help="Port to listen for incoming messages")
-	parser.add_argument("-max_clients",type=int,default=5000,help="maximum number of clients connected to proxy")
+	parser.add_argument("-cloud_compute",action='append',help="a Cloud-Compute node to interact with",required=True)
+	parser.add_argument("-cloud_compute_port",type=int,default=5093,help="Port the Cloud-Compute nodes are listening on")
+	parser.add_argument("-max_clients",type=int,default=5000,help="maximum number of clients connected to compute")
 	parser.add_argument("-queue_size",type=int,default=5000,help="Size of interal processing queue")
+	parser.add_argument("-offloading_percentile",type=int,default=50,help="Percentil of request offloaded to central cloud")
+	parser.add_argument("-cached_percentile",type=int,default=20,help="Percentil of request answered from cache to central cloud(calculated after offloading)")
+
+
+	# general 
+	parser.add_argument("-cleaning_interval",type=int,default=2,help="interval in which broken connections got cleared",metavar='2')
 
 	# logging
 	parser.add_argument("-log",help="Redirect logs to a given file in addition to the console.",metavar='')
@@ -60,6 +73,29 @@ def main():
 		configure_logging(debug)
 		logging.debug("debug mode enabled")
 
+# 1. IoT devices send data to Proxy
+# (2. Proxy asks Authenticate for authentification) only some %
+# 3. Proxy forwards request to Compute
+# 4. a. Compute offloads into Cloud
+#    b. Compute computes locally 
+#    c. Compute answers with cached data 
+#
+#		|					*EDGE_CLOUD *					|				* PUBLIC CLOUD *
+#		|					*************					|				****************
+#		|		   	   #####################				|
+#		|		  	  /# 5092 AUTHENTICATE #				|4.a  ######################\
+# IoT--	|			2/ #####################			  --------# 5093 Cloud Compute # \
+#     |	|			/									 /	|	  ######################  \
+# IoT------##############				################/	|4.a  ######################   \#################
+#	  1 |  # 5090 PROXY #---------------# 5091 Compute #----------# 5093 Cloud Compute #----# 5094 Database #
+# IoT------##############   \   3   /	################\	|	  ######################   /#################
+#		|		   		 	 -------				     \	|4.a  ######################  /
+# IoT------##############   /  3    \	################  --------# 5093 Cloud Compute # /		
+#	  1 |  # 5090 PROXY #---------------# 5091 COMPUTE #	|	  ######################/
+# IoT------##############				################	|
+#															|
+										|
+
 	# define Interface against Proxy devices	
 	proxy_interface = ListenNetworkInterface(t_name="Proxy_Interface",
 					listen_host="0.0.0.0",
@@ -67,42 +103,127 @@ def main():
 					maximum_number_of_listen_clients=args.max_clients,
 					queque_maxsize=args.queue_size
 					)
+
+	# define cloud loadbalancer 
+	cloud_lb_host_list = []
+	for c in args.cloud_compute:
+		cloud_lb_host_list.append((c,args.cloud_compute_port))
+	# define Interface against Authenticate devices	
+	cloud_load_balancer = RoundRobinLoadBalancer(host_list=cloud_lb_host_list)
+	cloud_interface = SendNetworkInterface(t_name="Authenticate_Interface",
+											load_balancer=cloud_load_balancer,
+											queque_maxsize=args.queue_size)
+
+	# get pipeline for incoming traffic from Proxy to answer from cache
+	if args.cached_percentile < 0:
+		logging.error("{} Negative Percentile for cached response(-cached_percentile).".format(t_name))
+		sys.exit(1)
+	if args.cached_percentile > 100:
+		logging,error("{} Unable to set a Percentile greater 100. (-cached_percentile).".format(t_name))
+		sys.exit(1)
+	in_iot_cached_pl = queue.Queue(maxsize=args.queue_size) # maybe define helper function
+	proxy_interface.add_fork(pipeline=in_iot_cached_pl,probability=args.cached_percentile,pos=0)
+
+	# get pipeline for incoming traffic from Proxy to offload
+	if args.offloading_percentile < 0:
+		logging.error("{} Negative Percentile for offloading requests(-offloading_percentile).".format(t_name))
+		sys.exit(1)
+	if args.offloading_percentile > 100:
+		logging,error("{} Unable to set a Percentile greater 100. (-offloading_percentile).".format(t_name))
+		sys.exit(1)
+	in_iot_offloading_pl = queue.Queue(maxsize=args.queue_size) # maybe define helper function
+	proxy_interface.add_fork(pipeline=in_iot_offloading_pl,probability=args.offloading_percentile,pos=0)
 	
-	# get pipeline for incoming traffic
-	__,out_proxy_inf_pipeline = proxy_interface.get_fork(-1)
+	# get pipeline for incoming traffic from Proxy to local compute
+	__,in_iot_local_compute = proxy_interface.get_fork(-1)
+
+	# get pipeline for incoming traffic from Proxy
+	__,in_iot_local_compute = proxy_interface.get_fork(-1)
 
 	# get pipeline for traffic to send to Proxy instances
-	in_proxy_inf_pipeline = proxy_interface.get_after_work_pipeline()
+	out_proxy_pl = proxy_interface.get_after_work_pipeline()
 
-	ELEMENTS.append(proxy_interface)
-	proxy_interface.start()
+	# get pipeline for traffic to send to Cloud instances
+	out_cloud_pl = cloud_interface.get_after_work_pipeline()
 
-	for i in range(0,5):
-		w = Worker(out_proxy_inf_pipeline,
-					in_proxy_inf_pipeline,
-					ComputeProcess()
-					)
+
+	# initialize  workers
+	cache_process = CacheProcess()
+	for i in range(0,1):
+		w = Worker(
+			in_iot_cached_pl,
+			out_proxy_pl,
+			cache_process)
+		ELEMENTS.append(w)
+		w.start()
+	
+	offloading_process = ForwardingProcess("OffloadingProcess")
+	for i in range(0,2):
+		w = Worker(
+			in_iot_offloading_pl,
+			out_cloud_pl,
+			offloading_process)
+		ELEMENTS.append(w)
+		w.start()
+	
+	local_compute_process = LocalComputeProcess()
+	for i in range(0,3):
+		w = Worker(
+			in_iot_local_compute,
+			out_proxy_pl,
+			local_compute_process)
 		ELEMENTS.append(w)
 		w.start()
 
+	forward_offloading_process = ForwardingProcess("ForwardOffloadingProcess")
+	for i in range(0,1):
+		w = Worker(
+			in_iot_local_compute,
+			out_proxy_pl,
+			forward_offloading_process)
+		ELEMENTS.append(w)
+		w.start()
+
+	# start networking
+	ELEMENTS.append(proxy_interface)
+	proxy_interface.start()
+	ELEMENTS.append(cloud_interface)
+	cloud_interface.start()
+
 	while RUNNING:
-		proxy_interface.clean(1)
-		time.sleep(2)
+		proxy_interface.clean(timeout=1)
+		cloud_interface.clean(timeout=1)
+		time.sleep(args.cleaning_interval)
 
 	proxy_interface.join()
+	cloud_interface.join()
 	sys.exit(0)
 
-class ComputeProcess(Process):
+class CacheProcess(Process):
+
+	t_name = "CacheProcess"
 
 	def __init__(self):
-		self.cpu = CPU()
+		vm_bytes=1024*2000
+		self.vm = VM(method="zero-one",vm_bytes=vm_bytes)
+		self.children["VM"]=self.vm
 		Process().__init__()
 
-	def __str__(self):
-		return "ComputeProcess"
+	def execute(self,device_id,request_id):
+		self.vm.utilize_vm()
+
+
+class LocalComputeProcess(Process):
+
+	t_name = "LocalComputeProcess"
+
+	def __init__(self):
+		self.cpu = CPU(method="nsqrt",max_ops=5)
+		self.children["CPU"]=self.cpu
+		Process().__init__()
 
 	def execute(self,device_id,request_id):
-		self.cpu.c_utilize_cpu_sqrt(30)
+		self.cpu.utilize_cpu()
 
 
 if __name__ == '__main__':
--- ms-proxy.py
+++ ms-proxy.py
@@ -5,10 +5,11 @@ from listen_network_interface import ListenNetworkInterface
 from send_network_interface import SendNetworkInterface
 from worker import Worker
 from load_balancer import RoundRobinLoadBalancer
-from process import Process
+from process import Process, ForwardingProcess
 
 RUNNING = True # controls main loop
 ELEMENTS = []
+t_name = "MS-PROXY"
 
 def configure_logging(debug,filename=None):
 	if filename is None:
@@ -37,16 +38,23 @@ def sig_int_handler(signal, frame):
 	
 def main():
 	global ELEMENTS
+	global __t_name
 	signal.signal(signal.SIGINT, sig_int_handler)
 
 	parser = argparse.ArgumentParser()
 	
 	# proxy
 	parser.add_argument("-listen_port",type=int, default=5090,help="Port to listen for incoming messages")
+	parser.add_argument("-authenticate",action='append',help="a Compute node to interact with",required=True)
+	parser.add_argument("-authenticate_port",type=int,default=5092,help="Port the compute nodes are listening on")
+	parser.add_argument("-authentication_percentile",type=int,default=10,help="a Compute node to interact with")
 	parser.add_argument("-compute",action='append',help="a Compute node to interact with",required=True)
 	parser.add_argument("-compute_port",type=int,default=5091,help="Port the compute nodes are listening on")
 	parser.add_argument("-max_clients",type=int,default=5000,help="maximum number of clients connected to proxy")
 	parser.add_argument("-queue_size",type=int,default=5000,help="Size of interal processing queue")
+
+	# general 
+	parser.add_argument("-cleaning_interval",type=int,default=2,help="interval in which broken connections got cleared",metavar='2')
 
 	# logging
 	parser.add_argument("-log",help="Redirect logs to a given file in addition to the console.",metavar='')
@@ -65,95 +73,137 @@ def main():
 		configure_logging(debug)
 		logging.debug("debug mode enabled")
 
+# 1. IoT devices send data to Proxy
+# (2. Proxy asks Authenticate for authentification) only some %
+# 3. Proxy forwards request to Compute
+# 4. a. Compute offloads into Cloud
+#    b. Compute computes locally 
+#    c. Compute answers with cached data 
+#
+#		|					*EDGE_CLOUD *					|				* PUBLIC CLOUD *
+#		|					*************					|				****************
+#		|		   	   #####################				|
+#		|		  	  /# 5092 AUTHENTICATE #				|4.a  ######################\
+# IoT--	|			2/ #####################			  --------# 5093 Cloud Compute # \
+#     |	|			/									 /	|	  ######################  \
+# IoT------##############				################/	|4.a  ######################   \#################
+#	  1 |  # 5090 PROXY #---------------# 5091 Compute #----------# 5093 Cloud Compute #----# 5094 Database #
+# IoT------##############   \   3   /	################\	|	  ######################   /#################
+#		|		   		 	 -------				     \	|4.a  ######################  /
+# IoT------##############   /  3    \	################  --------# 5093 Cloud Compute # /		
+#	  1 |  # 5090 PROXY #---------------# 5091 COMPUTE #	|	  ######################/
+# IoT------##############				################	|
+#															|
+	
+
 	# define Interface against IoT devices	
-	network_interface1 = ListenNetworkInterface(t_name="IoT_Device_Interface",
+	iot_interface = ListenNetworkInterface(t_name="IoT_Device_Interface",
 					listen_host="0.0.0.0",
 					listen_port=args.listen_port,
 					maximum_number_of_listen_clients=args.max_clients,
-					queque_maxsize=args.queue_size
-					)
+					queque_maxsize=args.queue_size)	
+	# setup pipeline to authentication, for 10% of all request comming from IoT devices
+	if args.authentication_percentile < 0:
+		logging.error("{} Negative Percentile for Authentication requests(-authentication_percentile).".format(t_name))
+		sys.exit(1)
+	if args.authentication_percentile > 100:
+		logging,error("{} Unable to set a Percentile greater 100. (-authentication_percentile).".format(t_name))
+		sys.exit(1)
+	iot_interface.add_fork(pipeline=in_iot_out_authentication_pl,probability=args.authentication_percentile,pos=0)
+	in_iot_out_authentication_pl = queue.Queue(maxsize=args.queue_size) # maybe define helper function
+	# get pipeline for incoming traffic from IoT devices
+	__,in_iot_out_compute_pl = iot_interface.get_fork(-1)
 
-	# get pipeline for incoming traffic
-	__,out_iot_inf_pipeline = network_interface1.get_fork(-1)
+	# define authenticate loadbalancer 
+	authenticate_lb_host_list = []
+	for c in args.authenticate:
+		authenticate_lb_host_list.append((c,args.authenticate_port))
+	# define Interface against Authenticate devices	
+	authenticate_load_balancer = RoundRobinLoadBalancer(host_list=authenticate_lb_host_list)
+	authentication_interface = SendNetworkInterface(t_name="Authenticate_Interface",
+											load_balancer=authenticate_load_balancer,
+											queque_maxsize=args.queue_size)
+	# get pipeline for incoming traffic from authentication
+	__,in_authentication_out_compute_pl = authentication_interface.get_fork(-1)
+
+	# define compute loadbalancer 
+	compute_lb_host_list = []
+	for c in args.compute:
+		compute_lb_host_list.append((c,args.compute_port))
+	compute_load_balancer = RoundRobinLoadBalancer(host_list=compute_lb_host_list)
+	# define Interface against Compute nodes
+	compute_interface = SendNetworkInterface(t_name="Compute_Interface",
+											load_balancer=compute_load_balancer,
+											queque_maxsize=args.queue_size)
+	# get pipeline for incoming traffic from compute
+	__,in_compute_out_iot_pl = authentication_interface.get_fork(-1)
+	
 	
 	# get pipeline for traffic to send to IoT devices
-	in_iot_inf_pipeline = network_interface1.get_after_work_pipeline()
-
-	# define loadbalancer 
-	lb_host_list = []
-	for c in args.compute:
-		lb_host_list.append((c,args.compute_port))
-	round_robin_load_balancer = RoundRobinLoadBalancer(host_list=lb_host_list)
-	
-	# define Interface against Compute nodes
-	network_interface2 = SendNetworkInterface(t_name="Compute_Interface",
-											load_balancer=round_robin_load_balancer,
-											queque_maxsize=args.queue_size)
-
-	# get pipeline for traffic come from compute instances
-	__,out_compute_inf_pipeline = network_interface2.get_fork(-1)
+	out_iot_pl = iot_interface.get_after_work_pipeline()
+	# get pipeline for traffic to send to authenticate
+	out_authentication_pl = authentication_interface.get_after_work_pipeline()
 	# get pipeline for traffic to send to Compute instances
-	in_compute_inf_pipeline = network_interface2.get_after_work_pipeline()
+	out_compute_pl = compute_interface.get_after_work_pipeline()
 
-	# initialize all workers
-	proxy_to_comp = ProxyToCOmputeProcess()
-	for i in range(0,5):
+	# initialize  workers
+	proxy_to_authentication = ForwardingProcess("ProxyToAuthenticationProcess")
+	for i in range(0,1):
 		w = Worker(
-			out_iot_inf_pipeline,
-			in_compute_inf_pipeline,
-			proxy_to_comp)
+			in_iot_out_authentication_pl,
+			out_authentication_pl,
+			proxy_to_authentication)
+		ELEMENTS.append(w)
+		w.start()
+	
+	authentication_to_compute = ForwardingProcess("AuthenticationToProxyProcess")
+	for i in range(0,1):
+		w = Worker(
+			in_authentication_out_compute_pl,
+			out_authentication_pl,
+			authentication_to_compute)
 		ELEMENTS.append(w)
 		w.start()
 
-	comp_to_proxy = ComputeToProxyProcess()
-	for i in range(0,5):
+	proxy_to_comp = ForwardingProcess("ProxyToComputeProcess")
+	for i in range(0,1):
 		w = Worker(
-			out_compute_inf_pipeline,
-			in_iot_inf_pipeline,
+			in_iot_out_compute_pl,
+			out_compute_pl,
+			proxy_to_comp
+			)
+		ELEMENTS.append(w)
+		w.start()
+
+	comp_to_proxy = ForwardingProcess("ComputeToProxyProcess")
+	for i in range(0,1):
+		w = Worker(
+			in_compute_out_iot_pl,
+			out_iot_pl,
 			comp_to_proxy
 			)
 		ELEMENTS.append(w)
 		w.start()
 
 	# start networking
-	ELEMENTS.append(network_interface1)
-	network_interface1.start()
-	ELEMENTS.append(network_interface2)
-	network_interface2.start()
+	ELEMENTS.append(iot_interface)
+	iot_interface.start()
+	ELEMENTS.append(authentication_interface)
+	authentication_interface.start()
+	ELEMENTS.append(compute_interface)
+	compute_interface.start()
 
 	while RUNNING:
-		network_interface1.clean(1)
-		network_interface2.clean(1)
-		time.sleep(2)
+		iot_interface.clean(timeout=1)
+		authentication_interface.clean(timeout=1)
+		compute_interface.clean(timeout=1)
+		time.sleep(args.cleaning_interval)
 
-	network_interface2.join()
-	network_interface1.join()
+	iot_interface.join()
+	authentication_interface.join()
+	compute_interface.join()
 
 	sys.exit(0)
-
-
-class ComputeToProxyProcess(Process):
-
-	def __init__(self):
-		Process().__init__()
-
-	def __str__(self):
-		return "ProxyToCOmputeProcess"
-
-	def execute(self,device_id,request_id):
-		pass 
-
-class ProxyToCOmputeProcess(Process):
-
-	def __init__(self):
-		Process().__init__()
-
-	def __str__(self):
-		return "ProxyToCOmputeProcess"
-
-	def execute(self,device_id,request_id):
-		pass
-
 
 if __name__ == '__main__':
 	main()
