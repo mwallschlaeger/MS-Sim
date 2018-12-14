e6d845afe134cb72d27ea493b42ac5625d8b8f1c
tibut <marcel.wallschlaeger@tu-berlin.de>
2018-12-14 14:32:34 +0100

    process now inherits from MSSimObject and suports t_name and metrics

--- process.py
+++ process.py
@@ -1,12 +1,27 @@
+from sim import MSSimObject
 
-class Process():
+class Process(MSSimObject):
+
+	t_name = "AbstractProcess"
 
 	def __init__(self):
 		pass
 
 	def __str__(self):
-		return "_Process"
+		return self.t_name
 
 	# Abstract function	
 	def execute(self,device_id,request_id):
 		pass
+
+class ForwardingProcess(Process):
+
+	t_name = "ForwardingProcess"
+
+	def __init__(self,t_name):
+		self.t_name = t_name
+		self.metrics["forwarded_packets"] = 0
+
+
+	def execute(self,device_id,request_id):
+		self.metrics["forwarded_packets"] += 1
