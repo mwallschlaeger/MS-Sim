9e4d8808fac1373b592153c827b03d1bcf85ec1d
tibut <marcel.wallschlaeger@tu-berlin.de>
2018-12-14 14:33:28 +0100

    removed __ prefix

--- sim.py
+++ sim.py
@@ -1,30 +1,36 @@
 
 
-class MS-Sim-Object():
+class MSSimObject():
+
+	t_name = "MS-Sim-Object"
+	conf = {}
+	metrics = {}
+	children = {}
 
 	def __init__(self):
-		self.__t_name = {}
-		self.__conf = {}
-		self.__metrics = {}
-		self.__children = {}
+		pass
 
-	def __get_metrics(self):
-		return self.__metrics
+	def get_metrics(self):
+		return self.metrics
 
-	def __get_children(self):
-		return self.__children
+	def get_children(self):
+		return self.children
 
-	def __get_conf(self):
-		return self.__conf
+	def get_conf(self):
+		return self.conf
 
-	def __get_t_name(self):
-		return self.__t_name
+	def get_t_name(self):
+		return self.t_name
 
-	def __get_leaf(self):
+	def get_leaf(self):
 		leaf = {}
-		leaf["conf"] = self.__conf
-		leaf["metrics"] = self.__metrics
-		leaf["children"] =self.__children
+		leaf["name"] = self.t_name
+		leaf["conf"] = self.conf
+		leaf["metrics"] = self.metrics
+		leaf["children"] =self.children
 		for c in children:
-			leaf["children"][c.__get_t_name()] = c.__get_leaf()
-		
+			leaf["children"][c.get_t_name()] = c.get_leaf()
+
+	def __reset_metrics(self):
+		for k,v in leaf["metrics"].items():
+			v = 0
