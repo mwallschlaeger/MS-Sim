3f6c39e6bef87ea2e6f7fadd993cd0f32cb65931
tibut <marcel.wallschlaeger@tu-berlin.de>
2018-12-21 12:28:59 +0100

    refectoring, catching exceptions, introducing conf,metrics,children,t_name

--- connection_handler.py
+++ connection_handler.py
@@ -1,15 +1,19 @@
 import queue, time, logging
+from sim import MSSimObject
 
-class ConnectionHandler():
+class ConnectionHandler(MSSimObject):
 	# TODO IMPLEMENT TIMEOUT
 
 	def __init__(self):
-		self.connections = {}
-		self.sorted_connections = []
-		
+		super().__init__()
 
+		self.connections = {}
+		self.t_name = "ConnectionHandler"
+		self.metrics["deleted_connections"] = 0
+		self.metrics["cleaned_connections"] = 0
+		self.metrics["key_errors"] = 0 # race condition
 	def __str__(self):
-		return "ConnectionManager"
+		return self.t_name
 
 	def add_connection(self,socket,address,device_id,request_id):
 		t_now = time.time()
@@ -19,7 +23,6 @@ class ConnectionHandler():
 		conn['address'] = address
 		conn['socket'] = socket
 		conn['device_id'] = device_id
-
 		self.connections[request_id] = conn
 
 	def get_next_connection_socket(self,device_id,request_id):
@@ -31,9 +34,14 @@ class ConnectionHandler():
 		 return s
 
 	def delete_connection(self,device_id,request_id):
-		del self.connections[request_id]
+		try:
+			del self.connections[request_id]
+			self.metrics["deleted_connections"] += 1
+		except KeyError:
+			self.metrics["key_errors"] += 1
 
-	def delete_all_connections():
+
+	def delete_all_connections(self):
 		keys = list(self.connections.keys())
 		for k in keys:
 			self.delete_connection("",k)
@@ -48,14 +56,18 @@ class ConnectionHandler():
 			sockets.append(self.connections[request_id]["socket"])
 		return sockets	
 
-	def close_connections(self):
+	def close_all_connections(self):
 		for k in self.connections.keys():
-			self.connections[k]['socket'].close()
+			self.connections[k]["socket"].close()
 
 	def clean(self, timeout):
 		t_now = time.time()
 		keys = list(self.connections.keys())
 		for k in keys:
-			if (t_now - self.connections[k]['t_s']) > timeout:
-				self.delete_connection("",k)
-
+			try:
+				if (t_now - self.connections[k]["t_s"]) > timeout:
+					self.metrics["cleaned_connections"] += 1
+					self.connections[k]["socket"].close()
+					self.delete_connection("",k)
+			except KeyError:
+				self.metrics["key_errors"] += 1
